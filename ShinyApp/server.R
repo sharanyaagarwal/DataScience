ea6c2a8cacf12817faa57eb6521a064699529632
Sharanya Agarwal <sharanya.agarwal@gmail.com>
2020-06-22 19:41:00 -0700

    Created the Shiny dashboard and added tabs with two value boxes to the introduction tab

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -8,19 +8,35 @@
 #
 
 library(shiny)
+library(dplyr)
+library(ggplot2)
+library(googleVis)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+shinyServer(function(input, output){
+    
+    output$table <- DT::renderDataTable({
+        DT::datatable(Startup_Data, options = list(paging = FALSE, scrollX = TRUE)) %>% DT::formatStyle(names(Startup_Data))
     })
-
+    
+    success <- nrow(filter(Startup_Data, Startup_Data$`Dependent-Company Status` == "Success"))
+    failed <- nrow(filter(Startup_Data, Startup_Data$`Dependent-Company Status` == "Failed"))
+    
+    output$successRate <- renderInfoBox({
+        infoBox(
+            "Startup Success Rate",
+            paste0((success * 100) / (failed+success), "%"), 
+            icon = icon("thumbs-up"),
+            color = 'orange'
+        )
+    })
+    
+    output$numCompanies <- renderInfoBox({
+        infoBox(
+            "Number of Companies",
+            nrow(Startup_Data), 
+            icon = icon("list"),
+            color = 'purple'
+        )
+    })
 })
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -1,25 +1,30 @@
 
 library(shiny)
+library(googleVis)
+library(shinydashboard)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+shinyUI(dashboardPage( 
+    dashboardHeader( title = "Shiny App"
+    ), 
+    dashboardSidebar(
+        sidebarUserPanel(
+            name = "Sharanya Agarwal", image = '0.jpg'
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        sidebarMenu(
+            menuItem("Introduction", tabName = "Intro", icon = icon("map")), 
+            menuItem("Factors", tabName = "Factors", icon = icon("map")), 
+            menuItem("Statistics", tabName = "Statistics", icon = icon("map")), 
+            menuItem("Data", tabName = "Data", icon = icon("database"))
+        )
+    ), 
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "Intro",
+                    fluidRow(valueBoxOutput("numCompanies"), valueBoxOutput("successRate"))),
+            tabItem(tabName = "Factors", "find out what factors affect the success rate"),
+            tabItem(tabName = "Statistics", "detail about what correlation is"),
+            tabItem(tabName = "Data", fluidRow(box(DT::dataTableOutput("table"), width = 12)))
         )
     )
 ))
